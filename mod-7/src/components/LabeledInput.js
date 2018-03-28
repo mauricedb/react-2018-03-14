c15c766c9f768ec10f795e2ce34d37c4e7368a7d
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-03-28 15:21:37 +0200

    CRUD forms code

--- mod-7/src/components/LabeledInput.js
+++ mod-7/src/components/LabeledInput.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class LabeledInput extends Component {
+class LabeledInput extends PureComponent {
+  onChange = e => {
+    const { name, valueChanged } = this.props;
+    const { value } = e.target;
+    valueChanged({
+      name,
+      value
+    });
+  };
   render() {
-    const { children, defaultValue, name } = this.props;
+    const { children, value, name, valueChanged, ...props } = this.props;
 
     return (
       <div>
         <label>
           {children}
-          <input
-            type="text"
-            name={name}
-            defaultValue={defaultValue}
-          />
+          <input type="text" {...props} value={value} onChange={this.onChange} />
         </label>
       </div>
     );
--- mod-7/src/components/PersonForm.js
+++ mod-7/src/components/PersonForm.js
@@ -2,27 +2,72 @@ import React, { Component } from 'react';
 import LabeledInput from './LabeledInput';
 
 class PersonForm extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    isValid: false,
+    errors: []
+  };
   submitPerson = e => {
     e.preventDefault();
 
-    console.log(e)
-    const person = {
-      firstName: e.target.elements.firstName.value,
-      lastName: e.target.elements.lastName.value
-    };
+    console.log(e);
+    // const person = {
+    //   firstName: yh.firstName.value,
+    //   lastName: e.target.elements.lastName.value
+    // };
+
+    const person = this.state;
 
     alert(JSON.stringify(person));
   };
+
+  valueChanged = e => {
+    const newState = { [e.name]: e.value };
+    this.setState(newState, () => {
+      const { firstName, lastName } = this.state;
+      const errors = [];
+
+      if (!firstName || !lastName) {
+        if (!firstName){
+          errors.push("Voornaam is verplicht!")
+        }
+        if (!lastName){
+          errors.push("Achternaam is verplicht!")
+        }
+        this.setState({ isValid: false, errors });
+      } else {
+        this.setState({ isValid: true, errors });
+      }
+    });
+  };
+
   render() {
+    const { firstName, lastName, isValid, errors } = this.state;
+
     return (
       <form onSubmit={this.submitPerson}>
         <h2>Person</h2>
+
+        <ul>{errors.map(er => <li key={er}>{er}</li>)}</ul>
         <div>
-          <LabeledInput defaultValue="Maurice" name="firstName">Voornaam:</LabeledInput>
-          <LabeledInput defaultValue="de Beijer" name="lastName">Achternaam</LabeledInput>
+          <LabeledInput
+            value={firstName}
+            name="firstName"
+            valueChanged={this.valueChanged}
+          >
+            Voornaam:
+          </LabeledInput>
+          <LabeledInput
+            value={lastName}
+            name="lastName"
+            valueChanged={this.valueChanged}
+          >
+            Achternaam
+          </LabeledInput>
         </div>
         <div>
-          <button>Bewaar</button>
+          <button disabled={!isValid}>Bewaar</button>
         </div>
       </form>
     );
